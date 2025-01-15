c949f39ab69d1b3217f35758c3a46f99de91fe69
Bradley Reynolds <bradley.reynolds@darbia.dev>
2025-01-15 01:31:11 +0000

    Init project
    Signed-off-by: GitHub <noreply@github.com>

---
+++ test/letsbuilda/imsosorrybutinparentheses_test.clj
@@ -0,0 +1,7 @@
+(ns letsbuilda.imsosorrybutinparentheses-test
+  (:require [clojure.test :refer :all]
+            [letsbuilda.imsosorrybutinparentheses :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
